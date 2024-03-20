8e569827d2ad336d3b96cc4545daba149e069db4
Syrgak <Kary6ekov.s@gmail.com>
2024-03-20 17:02:05 +0100

    steps 2.2, 2.3, 2.4 have been completed

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -10,9 +10,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.TelegramBotApplication;
+import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.service.NotificationTaskService;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -21,6 +28,8 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+    @Autowired
+    private NotificationTaskService notificationTaskService;
 
     @PostConstruct
     public void init() {
@@ -35,6 +44,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             if (update.message() != null && update.message().text().equals("/start")) {
                 handleStartCommand(update);
             }
+            if (compareMessage(update)) {
+                saveTask(update);
+            } else {
+                sendMessage("Запрос не правильно задан", update.message().chat().id());
+            }
 
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
@@ -45,10 +59,45 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         Long chatId = update.message().chat().id();
         sendMessage(messageText, chatId);
     }
+    //  ---- проверяет message
+    //  ---- если message соответствует к форме записи задачи возврашает true
+    //  ---- иначе false
+    //  ---- Формат проверки (dd.MM.yyyy HH:mm task text)
+    private boolean compareMessage(Update update) {
+        String massage = update.message().text();
+        Pattern pattern = Pattern.compile("^([0-9.:\\s]{16})(\\s)([\\W+]+)");
+        Matcher matcher = pattern.matcher(massage);
+        return matcher.matches();
+    }
 
+//    ---- сохраняет task в базу данных
+    private void saveTask(Update update) {
+        NotificationTask notificationTask = new NotificationTask();
+        String message = update.message().text();
+        Pattern pattern = Pattern.compile("^([0-9.:\\s]{16})(\\s)([\\W+]+)");
+        Matcher matcher = pattern.matcher(message);
+
+        if (matcher.find()) {
+            notificationTask.setSendDate(LocalDateTime.parse(matcher.group(1),
+                    DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
+            );
+            notificationTask.setMessage(matcher.group(3));
+        }
+
+        notificationTask.setChatId(update.message().chat().id());
+        notificationTaskService.save(notificationTask);
+    }
+
+
+    //    ----
     private void sendMessage(String messageText, Long chatId) {
         SendMessage message = new SendMessage(chatId, messageText);
         SendResponse response = telegramBot.execute(message);
+    }
+
+    private void processMessage(Update update) {
+        String message = update.message().text();
+
     }
 
 }
--- src/main/resources/liquibase/scripts/coursework.sql
+++ src/main/resources/liquibase/scripts/coursework.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS notification_task (
     chat_id BIGINT NOT NULL UNIQUE,
     send_date TIMESTAMP NOT NULL
 )
+
+-- changeset karybekov:2
+ALTER TABLE notification_task
+ADD COLUMN message VARCHAR(255)
+
+-- changeset karybekov:3
+ALTER TABLE notification_task DROP CONSTRAINT notification_task_chat_id_key
